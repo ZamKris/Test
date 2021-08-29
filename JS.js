e36fff0478c7cdfb5985440e0bef80f94005eb34
Shyam Krishnan <48869073+ZamKris@users.noreply.github.com>
2021-08-30 00:31:32 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,18 @@
+function selectnav(selection)
+{
+    let sel = document.getElementById(selection);
+
+    switch(selection)
+    {
+        case 'Home':
+                    break;
+        case 'Portfolio':
+                    break;
+        case 'About': 
+                    break;
+        case 'Contact':   
+                    break;
+                
+        default:;
+    }
+}
